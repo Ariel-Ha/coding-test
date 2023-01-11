02c838efeaf852a9ed3575d6fdf0a7d4fad1d32c
YeonJin55 <gkduswls1025@naver.com>
2023-01-11 23:27:49 +0900

    feat : 5-1 practice

---
+++ 5-DFS_BFS/5-1_스택_예제.py
@@ -0,0 +1,14 @@
+stack = []
+
+# 삽입5 - 삽입2 - 삽입3 - 삽입7 - 삭제 - 삽입1 - 삽입4 - 삭제
+stack.append(5)
+stack.append(2)
+stack.append(3)
+stack.append(7)
+stack.pop()
+stack.append(1)
+stack.append(4)
+stack.pop()
+
+print(stack) # 최하단 원소부터 출력
+print(stack[::-1]) # 최상단 원소부터 출력
